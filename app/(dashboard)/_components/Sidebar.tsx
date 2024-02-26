86aee487ac0be3097f7e02f06e9af32b5592be4e
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-02-26 23:57:32 +0100

    3- Layout created!

---
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="w-full h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6"></div>
+    </aside>
+  );
+};
+
+export default Sidebar;
