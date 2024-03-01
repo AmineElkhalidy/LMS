43b3ca3c856551891686fa26b56af24aa3234e3e
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-03-01 15:43:03 +0100

    7- Course UI added!

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+  return (
+    <div className="p-6">
+      <Link href="/teacher/create">
+        <Button className="bg-sky-700 duration-300 hover:bg-sky-900">
+          New Course
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default CoursesPage;
