1d5b8b4a5775c5a8c2de69a4dabd939b6e03aba9
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-03-01 15:14:47 +0100

    6- Dynamic layout

--- components/NavbarRoutes.tsx
+++ components/NavbarRoutes.tsx
@@ -1,11 +1,39 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 const NavbarRoutes = () => {
+  const pathname = usePathname();
+
+  const isTeacherPage = pathname?.startsWith("/teacher");
+  const isPlayerPage = pathname?.includes("/chapter");
+
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isTeacherPage || isPlayerPage ? (
+        <Link href="/">
+          <Button variant="ghost">
+            <LogOut className="w-4 h-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
+      ) : (
+        <Link href="/teacher/courses">
+          <Button
+            size="sm"
+            variant="ghost"
+            // className="bg-sky-700 duration-300 hover:bg-sky-900"
+          >
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
