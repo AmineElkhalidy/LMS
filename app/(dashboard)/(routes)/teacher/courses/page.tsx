6c70316437b7f63e416a91b4df78ee0f84d34142
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-06-07 09:56:39 +0100

    30- Course data table added!

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,15 +1,37 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
+      <DataTable columns={columns} data={courses} />
+
+      {/* <Link href="/teacher/create">
         <Button className="bg-sky-700 duration-300 hover:bg-sky-900">
           New Course
         </Button>
-      </Link>
+      </Link> */}
     </div>
   );
 };
