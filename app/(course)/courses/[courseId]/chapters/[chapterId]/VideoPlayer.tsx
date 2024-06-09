86c00664b06b96f01c8d6a9653861198953be215
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-06-09 15:24:55 +0100

    35- Chapter video player logic added!

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/VideoPlayer.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+import MuxPlayer from "@mux/mux-player-react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Loader2, Lock } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useConfettiStore } from "@/hooks/useConfettiStore";
+
+interface Props {
+  chapterId: string;
+  title: string;
+  courseId: string;
+  nextChapterId: string;
+  playbackId: string;
+  isLocked: boolean;
+  completeOnEnd: boolean;
+}
+const VideoPlayer = ({
+  chapterId,
+  title,
+  courseId,
+  nextChapterId,
+  playbackId,
+  isLocked,
+  completeOnEnd,
+}: Props) => {
+  const [isReady, setIsReady] = useState(false);
+
+  return (
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm">This chapter is lcoked</p>
+        </div>
+      )}
+
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isReady && "hidden")}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={() => {}}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
+    </div>
+  );
+};
+
+export default VideoPlayer;
--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const CoursePageDetails = () => {
-  return <div className=""></div>;
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) return redirect("/");
+  return redirect(`/courses/${course.id}/chapters/${course.chapters[0]?.id}`);
 };
 
-export default CoursePageDetails;
+export default CourseIdPage;
