b77b94962c9214dde0a725fa6f4dbbc5a9611fb0
Amine Elkhalidy <a.amineelkhalidy@gmail.com>
2024-06-09 18:43:35 +0100

    39- Stripe integration

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
@@ -2,7 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 interface Props {
   courseId: string;
@@ -10,8 +12,22 @@ interface Props {
 }
 
 const CourseEnrollButton = ({ courseId, price }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(response?.data?.url);
+    } catch {
+      toast.error("Something went wrong!");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Button size="sm" className="w-full md:w-auto">
+    <Button size="sm" className="w-full md:w-auto" disabled={isLoading}>
       Enroll for {formatPrice(price)}
     </Button>
   );
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/search", "/api/uploadthing"],
+  publicRoutes: ["/", "/search", "/api/uploadthing", "/api/webhook"],
   // Routes that can always be accessed, and have
   // no authentication information
 });
